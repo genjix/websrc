75637805dbded4b6fbbe2a2a32d4d8dc154f5e6a
Your Name <aphidia@hotmail.com>
2010-08-14 17:31:32 +0100

    added viewall webpage. removed security risk (save.php) formatted output of parse.php by adding newline disabled horizontal scrollbars which screw with my page.

---
+++ pago/pago.php
@@ -0,0 +1,48 @@
+<h3>pages</h3>
+
+<?php
+function RemoveExtension($strName)  
+{  
+     $ext = strrchr($strName, '.');  
+
+     if($ext !== false)  
+     {  
+         $strName = substr($strName, 0, -strlen($ext));  
+     }  
+     return $strName;  
+}
+
+if ($handle = opendir('.'))
+{
+?>
+<p><table>
+<?php
+    $dir_array = array();
+    while($lyric_file = readdir($handle)) {
+        $dir_array[] = $lyric_file;
+    }
+    closedir($handle);
+    $step = 0;
+    foreach ($dir_array as $file) {
+        if ($ext = substr(strrchr($file, '.'), 1) != "jpg") 
+            continue;
+        if ($step == 0)
+            echo "  <tr>\n";
+        $art = RemoveExtension($file);
+        echo "    <td><a href=\"#$art\"><img src=\"pago/$file\" /></a></td>\n";
+        $step++;
+        if ($step >= 2) {
+            echo "  </tr>\n";
+            $step = 0;
+        }
+    }
+    if ($step < 2 && $step != 0)
+        echo "  </tr>\n";
+}
+?>
+</table></p>
+
+<div id="footer">
+  <a href="email:genjix@gmail.com">contact</a> &nbsp;
+  <a href="http://creativecommons.org/licenses/by-nc-sa/1.0/" title="View details of the license of this site, courtesy of Creative Commons.">cc</a>
+</div>
--- save.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$content = $_POST['content']; 
-$file = "articles/uberman"; 
-$Saved_File = fopen($file, 'w'); 
-fwrite($Saved_File, $content); 
-fclose($Saved_File); 
-?>
